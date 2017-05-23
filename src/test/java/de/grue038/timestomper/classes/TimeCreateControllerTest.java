4357c84ebcc532832d6939ccadb3ef913d2a9620
Marsetex <marcel.gruessinger@web.de>
2017-05-23 22:37:30 +0200

    Initial import.

---
+++ src/test/java/de/grue038/timestomper/classes/TimeCreateControllerTest.java
@@ -0,0 +1,26 @@
+package de.grue038.timestomper.classes;
+
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+
+public class TimeCreateControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test1() throws Exception {
+		assertThat(true, is(true));
+	}
+
+	@Test
+	public void test2() throws Exception {
+		assertThat(true, is(true));
+	}
+
+}
