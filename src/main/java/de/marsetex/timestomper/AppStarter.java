02b648fedeb9a46db99f2555e6406817ba9eda25
Marsetex <marcel.gruessinger@web.de>
2017-08-19 00:54:14 +0200

    Removed obsolete code.

--- src/main/java/de/marsetex/timestomper/AppStarter.java
+++ src/main/java/de/marsetex/timestomper/AppStarter.java
@@ -1,7 +1,6 @@
 package de.marsetex.timestomper;
 
 import de.marsetex.timestomper.gui.fx.FXStageInitializer;
-import javafx.application.Platform;
 
 /**
  * 
@@ -11,37 +10,7 @@ import javafx.application.Platform;
 public class AppStarter {
 
 	public static void main(String[] args) {
-
-		// Employee emp = new Employee();
-		// emp.setName("Pankaj");
-		// emp.setRole("CEO");
-		// emp.setInsertTime(new Date());
-		//
-		// // creating configuration object
-		// Configuration cfg = new Configuration();
-		// cfg.configure("hibernate.cfg.xml");// populates the data of the
-		// // configuration file
-		//
-		// // creating seession factory object
-		// SessionFactory factory = cfg.buildSessionFactory();
-		// Session session = factory.openSession();
-		// // start transaction
-		// Transaction sss = session.beginTransaction();
-		//
-		// // Save the Model object
-		// // session.save(emp);
-		// // Commit transaction
-		// sss.commit();
-		//
-		// Employee s = session.find(Employee.class, 1);
-		// // System.out.println(s.getName());
-		// session.close();
-
-		System.out.println("successfully saved");
-
 		new FXStageInitializer().initUserInterface(args);
-		Platform.exit();
-		System.exit(0);
 	}
 
 }
