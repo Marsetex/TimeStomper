9315bdee03a19d8b50d432e43d588b0886e2cc38
Marsetex <marcel.gruessinger@web.de>
2017-09-03 17:51:00 +0200

    Implemented DatabaseConnector.

---
+++ src/main/java/de/marsetex/timestomper/database/DatabaseConnector.java
@@ -0,0 +1,31 @@
+package de.marsetex.timestomper.database;
+
+public class DatabaseConnector {
+
+	public void test() {
+		// Employee emp = new Employee();
+		// emp.setName("Pankaj");
+		// emp.setRole("CEO");
+		// emp.setInsertTime(new Date());
+		//
+		// // creating configuration object
+		// Configuration cfg = new Configuration();
+		// cfg.configure("hibernate.cfg.xml");// populates the data of the
+		// // configuration file
+		//
+		// // creating seession factory object
+		// SessionFactory factory = cfg.buildSessionFactory();
+		// Session session = factory.openSession();
+		// // start transaction
+		// Transaction sss = session.beginTransaction();
+		//
+		// // Save the Model object
+		// // session.save(emp);
+		// // Commit transaction
+		// sss.commit();
+		//
+		// Employee s = session.find(Employee.class, 1);
+		// // System.out.println(s.getName());
+		// session.close();
+	}
+}
---
+++ src/test/java/de/marsetex/timestomper/database/DatabaseConnectorTest.java
@@ -0,0 +1,17 @@
+package de.marsetex.timestomper.database;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DatabaseConnectorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testTest() throws Exception {
+
+	}
+
+}
